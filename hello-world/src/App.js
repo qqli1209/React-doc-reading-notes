546f41bcf85ea4d20cbe5738c88efb4c01436fde
qqli1209 <lqq201194@163.com>
2017-10-15 11:53:47 +0800

    Clock component works

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+
+export default class App extends Component {
   render() {
     return (
       <div className="App">
@@ -17,5 +19,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,8 +1,13 @@
+import './index.css';
+
+import App from './App';
+import Clock from './Clock';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Clock />, document.getElementById('clock'));
+
 registerServiceWorker();
