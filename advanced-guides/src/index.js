aa3eb03e3096a5fac589ef83756f98b940acc497
qqli1209 <lqq201194@163.com>
2017-10-19 20:10:56 +0800

    add two new demo apps

--- advanced-guides/src/index.js
+++ advanced-guides/src/index.js
@@ -1,9 +1,22 @@
 import './index.css';
 
+import CustomTextInput, { AutoFocusTextInput } from './RefsAndTheDOM/CustomTextInput'
+
 import App from './App/App';
+import GrandParent from './RefsAndTheDOM/ExposingDOMRef';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WordAdder from './OptimizingPerformance/WordAdder';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<CustomTextInput />, document.getElementById('CustomTextInput'));
+
+ReactDOM.render(<AutoFocusTextInput />, document.getElementById('AutoFocusTextInput'));
+
+ReactDOM.render(<GrandParent />, document.getElementById('GrandParent'));
+
+ReactDOM.render(<WordAdder />, document.getElementById('WordAdder'));
+
 registerServiceWorker();
