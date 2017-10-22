0a0416d0bff9a2abb48f815398a077c78f250b84
qqli1209 <lqq201194@163.com>
2017-10-22 12:07:10 +0800

    finish chapter Error Boundaries in advanced guides

--- advanced-guides/src/index.js
+++ advanced-guides/src/index.js
@@ -3,6 +3,7 @@ import './index.css';
 import CustomTextInput, { AutoFocusTextInput } from './RefsAndTheDOM/CustomTextInput'
 
 import App from './App/App';
+import AppWithErrorBoundary from './ErrorBoundaries/AppWithErrorBoundary'
 import AppWithPortal from './Portals/AppWithPortal'
 import GrandParent from './RefsAndTheDOM/ExposingDOMRef';
 import React from 'react';
@@ -21,6 +22,8 @@ ReactDOM.render(<GrandParent />, document.getElementById('GrandParent'));
 ReactDOM.render(<WordAdder />, document.getElementById('WordAdder'));
 
 ReactDOM.render(<AppWithPortal />, document.getElementById('app-root'));
+
+ReactDOM.render(<AppWithErrorBoundary />, document.getElementById('AppWithErrorBoundary'));
 
 
 registerServiceWorker();
